94abcb2a507d836b609532df9a778877fa296ccf
Yaseen AlGailani <yaseen.algailani@gmail.com>
2022-12-13 10:10:35 +1100

    test: update

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -10,4 +10,18 @@ describe("Home", () => {
 
     expect(heading).toBeInTheDocument();
   });
+
+  it("renders three progress bars", () => {
+    render(<Home />);
+     const progressBars = screen.getAllByRole("progressbar");
+
+     expect(progressBars.length).toEqual(3);
+  });
+
+  it("renders four buttons", () => {
+    render(<Home />);
+    const buttons = screen.getAllByRole('button')
+
+    expect(buttons.length).toEqual(4);
+  });
 });
