49adc9e872d9ca013849fed9ede40c08aafe81ed
Yaseen AlGailani <yaseen.algailani@gmail.com>
2022-12-13 08:40:53 +1100

    feat: system dark mode for my eyes

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+      </Head>
+      <body className="bg-neutral-50 dark:bg-slate-800">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
